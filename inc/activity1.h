1167468a5a27dcd0e3318385489f2f283a3498a0
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-23 19:49:42 +0530

    Create activity1.h

---
+++ inc/activity1.h
@@ -0,0 +1,40 @@
+#ifndef __ACTIVITY1_H_
+#define __ACTIVITY1_H_
+/**
+ * @file activity1.h
+ * @author Charanya ()
+ * @brief Project to Blink an LED connected to AVR328 MCU GPIO Pin if Push button of the car seat is closed and heater switch is turned ON
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro Definitions
+ */
+#define ON 	(1)			/**< LED state HIGH */
+#define OFF	(0)			/**< LED state LOW */
+
+#define LED_ON_TIME     (200)  /**< LED ON time in milli seconds  */
+#define LED_OFF_TIME    (100)   /**< LED OFF time in milli seconds */
+
+/**
+ * Include files
+ */ 
+#include <avr/io.h>
+
+/**
+ * Function Definitions
+ */
+
+/**
+ * @brief Change the state of the LED Pin according to the state of the push button in car seat and heater switch
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void change_led_state(uint8_t state);
+
+
+#endif /** __ACTIVITY1_H_ */
