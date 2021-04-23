2bcf6a320292515c03abb5f7abc03176a58f4722
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-23 19:53:42 +0530

    Create project_config.h

---
+++ inc/project_config.h
@@ -0,0 +1,22 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * @file project_config.h
+ * @author Charanya ()
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro Definitions
+ */
+
+#define LED  (PORTB)     /**< LED Port Number */
+#define LED_PIN (PB0)    /**< LED Pin number  */
+
+
+#endif /* __PROJECT_CONFIG_H__ */
