62d4125b7923cb664565a6b7b28fb732af01fcaa
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-23 20:09:48 +0530

    Update user_utils.c

--- src/user_utils.c
+++ src/user_utils.c
@@ -9,3 +9,23 @@ void delay_ms(uint32_t delay_time)
 		_delay_ms(1);
 	}
 }
+
+
+/**
+ * @brief Initialize all the Peripherals and pin configurations
+ * 
+ */
+void peripheral_init(void)
+{
+	/* Configure LED Pin */
+	DDRB |= (ON <<LED_PIN);
+    DDRD &= (OFF<<BUTTON_PIN);
+    DDRC &= (OFF<<SWITCH_PIN);
+    BUTTON |= (ON <<BUTTON_PIN);
+    SWITCH |= (ON <<SWITCH_PIN);
+}
+
+void change_led_state(uint8_t state)
+{
+	LED |= (ON<<LED_PIN);
+}
