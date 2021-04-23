b811ccd96653902f2305ae4cd917da7d62fd98bf
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-23 23:46:49 +0530

    Update user_utils.c

--- src/user_utils.c
+++ src/user_utils.c
@@ -22,17 +22,33 @@ void peripheral_init(void)
 {
 	/* Configure LED Pin */
 	DDRB |= (ON <<LED_PIN);
+	
+	/* Configure Seat button Pin */
     DDRD &= (OFF<<BUTTON_PIN);
+	
+	/* Configure heater switch Pin */
     DDRC &= (OFF<<SWITCH_PIN);
+	
+	/* Convert pull up pin to pull down  nature  */
     BUTTON |= (ON <<BUTTON_PIN);
     SWITCH |= (ON <<SWITCH_PIN);
 }
+
+/**
+ * @brief if both the conditions(seat occupied and heater switch is ON) satisfied turn LED ON
+ * 
+ */
 
 void led_on(uint8_t state)
 {
 	LED |= (state<<LED_PIN);
 }
 
+
+/**
+ * @brief if both the conditions not satisfied turn LED ON
+ * 
+ */
 void led_off(uint8_t state)
 {
 	LED &= (state<<LED_PIN);
