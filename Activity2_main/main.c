b52486c1701d1a7aba209d28e9c692885faa31c3
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-29 21:21:12 +0530

    Update main.c

--- Activity2_main/main.c
+++ Activity2_main/main.c
@@ -1,3 +1,25 @@
+/**
+ * @file project2_main.c
+ * @author Charanya ()
+ * @brief Project to read analogue values from a temperature sensor (Potentiometer) 
+          and convert the obtained values to digital values
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "activity2.h"
+
+/**
+ * @brief Main function where the code execution starts
+ * 
+ * @return int Return 0 if the program completes successfully
+ * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
+ * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ */
+
 int main(void)
 {
 
