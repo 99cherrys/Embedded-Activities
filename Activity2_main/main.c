f779c5a02ad11b85b2bca874ed749c58d21bf123
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-29 21:23:36 +0530

    Update main.c

--- Activity2_main/main.c
+++ Activity2_main/main.c
@@ -2,7 +2,7 @@
  * @file project2_main.c
  * @author Charanya ()
  * @brief Project to read analogue values from a temperature sensor (Potentiometer) 
-          and convert the obtained values to digital values
+ * and convert the obtained values to digital values
  * @version 0.1
  * @date 2021-04-29
  * 
@@ -16,8 +16,8 @@
  * @brief Main function where the code execution starts
  * 
  * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ * 
+ * initialize ADC register and read analog values from the channel 5 by calling ReadADC function where conversion happens
  */
 
 int main(void)
