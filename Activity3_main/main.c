b45fc19a22a1423df7c16a2b44d5272edb368a60
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-30 21:13:05 +0530

    Update main.c

--- Activity3_main/main.c
+++ Activity3_main/main.c
@@ -24,13 +24,12 @@ int main()
     InitADC();
     PWM_init();
 
-    uint16_t temp=0; //initialize a temp variable to 0
-     uint16_t degree=0;
-
+    uint8_t temp=0; //initialize a temp variable to 0
+   
     while(1)
     {
         temp = ReadADC(5);
-        degree = heat(temp);
+        heat(temp);
         _delay_ms(200);
     }
 
