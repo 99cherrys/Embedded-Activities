1a422424a6b6cca0f46a6a6a00586e92f7fe87a7
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-05-01 01:34:31 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -1,3 +1,6 @@
+#ifndef __ACTIVITY4_H_
+#define __ACTIVITY4_H_
+
 #include <avr/io.h>
 
 uint8_t temp;
@@ -9,3 +12,5 @@ void heat(uint16_t temp);
 void InitADC();
 
 uint16_t ReadADC(uint16_t ch);
+
+#endif
