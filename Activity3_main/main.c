d882d0a082b9fc61c5f81eee6b324730a70d2f23
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-30 20:22:28 +0530

    Update main.c

--- Activity3_main/main.c
+++ Activity3_main/main.c
@@ -1,4 +1,23 @@
+/**
+ * @file project2_main.c
+ * @author Charanya ()
+ * @brief Project to Turn ON an LED at 200ms ON and 100 ms OFF Interval
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#include "activity3.h"
+
+/**
+ * @brief Main function where the code execution starts
+ * 
+ * @return int Return 0 if the program completes successfully
+ * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
+ * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ */
 
 int main()
 {
