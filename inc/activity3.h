1a1105c9166c9d8beaed82c6919faab10be576dd
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-05-01 01:42:16 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -17,14 +17,28 @@
  */ 
 #include <avr/io.h>
 
+// Global variable declaration
 uint8_t temp;
 
 void PWM_init();
 
+/**
+ * @brief to convert the temperature to required range and display 20/25/29/33 degree celcius
+ *
+ * @param data read from the potentiometer temperature sensor
+ */
 void heat(uint16_t temp);
 
+/**
+ * @brief to initialize ADC
+ */
 void InitADC();
 
+/**
+ * @brief to read ADC value
+ *
+ * @param channel number of ADC
+ */
 uint16_t ReadADC(uint16_t ch);
 
 #endif
